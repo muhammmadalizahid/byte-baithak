e67bfeaead952dd47e3ff49f157864f6ca9af1c3
Muhammad Ali Zahid <2024CS204@student.uet.edu.pk>
2026-01-29 13:02:34 +0500

    minor revisions

--- src/components/Collaborators.jsx
+++ src/components/Collaborators.jsx
@@ -29,14 +29,6 @@ const Collaborators = () => {
 leadership, technical awareness, and industry readiness
 among students.
             </p>
-            <div className="collaborator-link">
-              <a href="#" className="link-subtle">
-                Learn More
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M7 17L17 7M17 7H7M17 7V17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-              </a>
-            </div>
           </div>
 
           {/* Divider */}
@@ -56,14 +48,6 @@ among students.
               Google Developer Group On Campus at UET Lahore connects students with Google's 
               developer ecosystem, modern tech practices, and industry experts.
             </p>
-            <div className="collaborator-link">
-              <a href="#" className="link-subtle">
-                Learn More
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M7 17L17 7M17 7H7M17 7V17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-              </a>
-            </div>
           </div>
         </div>
 
